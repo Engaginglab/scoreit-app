63fe7d0a8176f1e67e28fc575733fe59764de597
Martin Kleinschrodt <martin@maklesoft.com>
2012-08-04 18:22:34 +0200

    Reset fields after login; Allow submit by pressing enter

--- source/TopBar.js
+++ source/TopBar.js
@@ -20,18 +20,25 @@ enyo.kind({
 	},
 	login: function() {
 		this.doLogin({username: this.$.username.getValue(), password: this.$.password.getValue()});
+		this.$.username.setValue("");
+		this.$.password.setValue("");
 	},
 	logout: function() {
 		this.doLogout();
+	},
+	keyupHandler: function(sender, event) {
+		if (event.keyCode == 13) {
+			this.login();
+		}
 	},
 	components: [
 		{kind: "Image", src: "assets/images/scoreit_white_nobounce_tiny.png", style: "height: 22px; margin: 3px 4px;"},
 		{name: "loginForm", style: "float: right;", components: [
 			{kind: "onyx.InputDecorator", classes: "always-focused", components: [
-				{kind: "onyx.Input", name: "username", placeholder: "Benutzername"}
+				{kind: "onyx.Input", name: "username", placeholder: "Benutzername", onkeyup: "keyupHandler"}
 			]},
 			{kind: "onyx.InputDecorator", classes: "always-focused", components: [
-				{kind: "onyx.Input", name: "password", type: "password", placeholder: "Passwort"}
+				{kind: "onyx.Input", name: "password", type: "password", placeholder: "Passwort", onkeyup: "keyupHandler"}
 			]},
 			{kind: "onyx.Button", content: "Login", classes: "onyx-negative", ontap: "login"}
 		]},
