1c7ca3bde7b921b481b5bdb3b75c16a30fb13a72
Martin Kleinschrodt <martin@maklesoft.com>
2012-08-04 18:19:15 +0200

    Refactor signup and login procedure; Give panels a fixed height to avoid resizing issues

--- source/App.js
+++ source/App.js
@@ -9,18 +9,41 @@ enyo.kind({
 	},
 	create: function() {
 		this.inherited(arguments);
+		this.loadUser();
+	},
+	showView: function(view) {
+		this.$.panels.setIndex(this.views[view]);
+	},
+	loadUser: function() {
+		var user = this.fetchUser();
+		if (user) {
+			this.$.loadingPopup.setText("Lade Daten für Benutzer " + user.username + "...");
+			this.$.loadingPopup.show();
+			scoreit.auth.user.detail(user.id, enyo.bind(this, function(sender, response) {
+				scoreit.user = response;
+				this.$.loadingPopup.hide();
+				this.userChanged();
+			}));
+		} else {
+			scoreit.user = null;
+			this.userChanged();
+		}
+	},
+	fetchUser: function() {
 		var user = null;
 		try {
 			user = JSON.parse(localStorage.getItem("user"));
 		} catch(e) {
 		}
-		scoreit.user = user;
-		this.updateUser();
+		return user;
 	},
-	showView: function(view) {
-		this.$.panels.setIndex(this.views[view]);
+	saveUser: function() {
+		localStorage.setItem("user", JSON.stringify(scoreit.user));
 	},
-	updateUser: function() {
+	deleteUser: function() {
+		localStorage.removeItem("user");
+	},
+	userChanged: function() {
 		this.$.topBar.setUser(scoreit.user);
 
 		if (scoreit.user) {
@@ -35,7 +58,8 @@ enyo.kind({
 		this.$.loadingPopup.show();
 		scoreit.signUp(event.data, enyo.bind(this, function(sender, response) {
 			this.$.loadingPopup.hide();
-			this.$.signedUpPopup.show();
+			this.$.alertPopup.setMessage("Dir wurde soeben eine Email and die Adresse " + event.data.email + " geschickt. Klicke auf den Link in der Email, um deinen Account zu aktiveren!");
+			this.$.alertPopup.show();
 		}));
 	},
 	hideSignedUpPopup: function() {
@@ -43,25 +67,22 @@ enyo.kind({
 	},
 	loginHandler: function(sender, event) {
 		scoreit.login(event.username, event.password, enyo.bind(this, function(success) {
-			localStorage.setItem("user", JSON.stringify(scoreit.user));
-			this.updateUser();
+			this.saveUser();
+			this.userChanged();
 		}));
 	},
 	logoutHandler: function() {
 		scoreit.user = null;
-		localStorage.removeItem("user");
-		this.updateUser();
+		this.deleteUser();
+		this.userChanged();
 	},
 	components: [
 		{kind: "TopBar", onLogin: "loginHandler", onLogout: "logoutHandler"},
-		{kind: "Panels", name: "panels", fit: true, style: "width: 850px; margin: 0 auto;", components: [
+		{kind: "Panels", name: "panels", style: "width: 850px; margin: 0 auto; height: 600px;", components: [
 			{kind: "FrontMatter", onSignUp: "signUpHandler"},
 			{kind: "GetStartedView"}
 		]},
 		{kind: "LoadingPopup"},
-		{kind: "onyx.Popup", name: "signedUpPopup", floating: true, centered: true, style: "right: auto;", components: [
-			{content: "Dir wurde soeben eine Email geschickt. Klicke auf den Link in der Email, um deinen Account zu aktiveren!"},
-			{kind: "onyx.Button", content: "OK", onclick: "hideSignedUpPopup"}
-		]}
+        {kind: "AlertPopup"}
 	]
 });
