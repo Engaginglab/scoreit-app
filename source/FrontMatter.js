09940292eceb6670bf41ed2a056ba144b5d74778
Martin Kleinschrodt <martin@maklesoft.com>
2012-08-21 05:00:09 +0200

    Couple of design changes

--- source/FrontMatter.js
+++ source/FrontMatter.js
@@ -10,13 +10,13 @@ enyo.kind({
 	components: [
 		{kind: "Scroller", classes: "enyo-fill", components: [
 			{classes: "frontmatter-content", components: [
-				{allowHtml: true, content: "There is a story <br> behind your numbers.",
+				{allowHtml: true, classes: "frontmatter-slogan", content: "There is a story <br> behind your numbers.",
 					style: "position: absolute; text-align: center; top: 40px; left: 10px; font-size: 15pt;"},
-				{content: "Discover it.", style: "position: absolute; text-align: center; top: 220px; left: 0px; font-size: 18pt;"},
-				{content: "And benefit!", style: "position: absolute; text-align: center; top: 250px; left: 320px; font-size: 20pt;"},
+				{content: "Discover it.", classes: "frontmatter-slogan", style: "position: absolute; text-align: center; top: 220px; left: 0px; font-size: 18pt;"},
+				{content: "And benefit!", classes: "frontmatter-slogan", style: "position: absolute; text-align: center; top: 250px; left: 320px; font-size: 20pt;"},
 				{style: "width: 300px; float: right; text-align: center;", components: [
-					{style: "font-size: 15pt; color: #BDE515;", content: "Noch nicht dabei?"},
-					{style: "font-size: 24pt; color: #C51616;", content: "Jetzt registrieren!"},
+					{style: "font-size: 15pt; color: #A6CB20;", content: "Noch nicht dabei?"},
+					{style: "font-size: 24pt; color: #333333;", content: "Jetzt registrieren!"},
 					{kind: "SignUpForm", style: "width: 100%;", onSubmit: "signUpHandler"}
 				]}
 			]}
